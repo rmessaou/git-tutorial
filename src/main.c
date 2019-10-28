a107734b6dddcab375f5bc92b6e07b06c193d0e7
Rayene Messaoud <ga27voz@mytum.de>
2019-10-28 16:12:39 +0100

    completing the git challenge to turn numbers into words

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include "espl_lib.h"
 
 int main(){
 
 printf("Hello ESPL\n");
-printf("This is a new print\n");
+char answer = 'y';
+int num = 0;
+while(answer == 'y'){
+	printf("please write a number: ");	
+	scanf("%d",&num);
+	char* word = num_to_words(num);
+	char tmp = getchar();
+	printf("=====================\nyour number is %s\n=====================\n", word);
+	printf("Do you want to cintinue [y]? or quit [q]?: ");
+	scanf("%c", &answer);
+}
+
 return 0;
 }
